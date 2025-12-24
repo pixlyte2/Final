b1ea4ad59f681290b5906bfd4146582f3591d4d0
pixlyte2 <pixlyt.e2@gmail.com>
2025-12-24 17:27:47 +0530

    files

--- server/models/User.js
+++ server/models/User.js
@@ -1,29 +1,9 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-      trim: true
-    },
-
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-      lowercase: true,
-      trim: true
-    },
-
-    password: {
-      type: String,
-      required: true
-    }
-  },
-  {
-    timestamps: true
-  }
-);
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: { type: String, unique: true },
+  password: String
+});
 
 module.exports = mongoose.model('User', userSchema);
